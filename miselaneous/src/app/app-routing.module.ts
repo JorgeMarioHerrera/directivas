c8df228939334e692f740017630ecc2e919b2550
jorge mario herrera vargas <jorgemario2pac@gmail.com>
2021-01-17 13:29:26 -0500

    Practica finalizada

--- miselaneous/src/app/app-routing.module.ts
+++ miselaneous/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
+
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'usuarios', component: UsuarioComponent},
+  {
+    path: 'usuario/:id',
+    component: UsuarioComponent,
+    children: USUARIO_ROUTES
+  },
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 
 ];
